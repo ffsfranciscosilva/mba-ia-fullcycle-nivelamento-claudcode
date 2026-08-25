ec95ce894ea2a91a31b87176d07c493ab96329af
ffsfranciscosilva <ffsfranciscosilva@gmnail.com>
2026-08-25 14:54:09 -0300

    Remove subproject

--- LangChain/1-fundamentos/2-init-chat-model.py
+++
@@ -1,8 +0,0 @@
-from langchain.chat_models import init_chat_model
-from dotenv import load_dotenv
-load_dotenv()
-
-gemini = init_chat_model(model="gemini-3.7-flash", model_provider="google_genai")
-answer_gemini = gemini.invoke("Hello, world!")
-
-print(answer_gemini.content)
--- LangChain/1-fundamentos/3-prompt-template.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.prompts import PromptTemplate
-
-template = PromptTemplate(
-    input_variables=["name"],
-    template="Hi, I`m {name}! Tell me a joke with my name!",
-)
-
-text = template.format(name="Francisco")
-print(text)
